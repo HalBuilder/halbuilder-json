ceb122c301a2a2947fd9ec8fe1e110e64f41ed80
Mark Derricutt <mark@talios.com>
2015-08-19 14:38:46 +1200

    Fix parser of array values.
    Fixes #4

--- src/main/java/com/theoryinpractise/halbuilder/json/JsonRepresentationReader.java
+++ src/main/java/com/theoryinpractise/halbuilder/json/JsonRepresentationReader.java
@@ -127,7 +127,7 @@ public class JsonRepresentationReader implements RepresentationReader {
         if(field.isArray()) {
             List<Object> arrayValues = new ArrayList<Object>(field.size());
             for(JsonNode arrayValue : field) {
-                arrayValues.add(ImmutableMap.copyOf(mapper.readValue(arrayValue.toString(), Map.class)));
+              arrayValues.add(!arrayValue.isContainerNode() ? arrayValue.asText() : ImmutableMap.copyOf(mapper.readValue(arrayValue.toString(), Map.class)));
             }
             resource.withProperty(fieldName, arrayValues);
         } else {
