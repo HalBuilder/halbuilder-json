9edf983a61f7992c02431203d8d93694b8135347
Sven-Torben Janus <sven-torben@sven-torben.de>
2016-09-13 14:28:44 +0200

    Uses COALESCE_LINKS instead of COALESCE_ARRAYS
    Fixes HalBuilder/halbuilder-json#18

---
+++ src/test/java/com/theoryinpractise/halbuilder/json/TestIssue18.java
@@ -0,0 +1,33 @@
+package com.theoryinpractise.halbuilder.json;
+
+
+import static org.testng.Assert.assertFalse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.theoryinpractise.halbuilder.api.Representation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class TestIssue18 {
+
+  @Test
+  public void test() throws IOException {
+
+    RepresentationFactory representationFactory = new JsonRepresentationFactory()
+        .withFlag(RepresentationFactory.COALESCE_LINKS);
+
+    Representation embedded = representationFactory.newRepresentation("http://localhost/embedded");
+    embedded.withLink("applicants", "http://localhost/applicants");
+    Representation representation = representationFactory.newRepresentation("http://localhost/self");
+    representation.withRepresentation("items", embedded);
+
+    String json = representation.toString(RepresentationFactory.HAL_JSON);
+    ObjectNode objectNode = (ObjectNode) new ObjectMapper().readTree(json);
+
+    assertFalse(objectNode.get("_embedded").get("items").get(0).get("_links").get("applicants").isArray());
+  }
+
+}
